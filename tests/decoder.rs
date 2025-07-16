3c2b3265baad58e612c2016989587d4799a09095
gliderkite <gliderkite@gmail.com>
2025-07-16 16:21:48 +0200

    Step - 5d Implement shortest path with Dijkstra

--- tests/decoder.rs
+++ tests/decoder.rs
@@ -1,9 +1,9 @@
 mod graph;
 
 use openlr::{
-    Bearing, CandidateNode, CandidateNodes, Coordinate, DecoderConfig, Fow, Frc, Length,
-    LineAttributes, PathAttributes, Point, RatingScore, decode_base64_openlr, find_candidate_lines,
-    find_candidate_nodes,
+    Bearing, CandidateLine, CandidateLines, CandidateNode, CandidateNodes, Coordinate,
+    DecoderConfig, Fow, Frc, Length, LineAttributes, PathAttributes, Point, RatingScore, Route,
+    decode_base64_openlr, find_candidate_lines, find_candidate_nodes, resolve_routes,
 };
 use test_log::test;
 
@@ -178,5 +178,86 @@ fn find_candidate_lines_001() {
             ],
             vec![(EdgeId(109783), None)]
         ]
+    );
+}
+
+#[test]
+fn resolve_routes_001() {
+    let graph: &NetworkGraph = &NETWORK_GRAPH;
+
+    let config = DecoderConfig {
+        max_node_distance: Length::from_meters(100.0),
+        max_bearing_difference: Bearing::from_degrees(90),
+        min_line_rating: RatingScore::from(800.0),
+        ..Default::default()
+    };
+
+    let first_lrp = Point {
+        coordinate: Coordinate {
+            lon: 13.46112,
+            lat: 52.51711,
+        },
+        line: LineAttributes {
+            frc: Frc::Frc6,
+            fow: Fow::SingleCarriageway,
+            bearing: Bearing::from_degrees(107),
+        },
+        path: Some(PathAttributes {
+            lfrcnp: Frc::Frc6,
+            dnp: Length::from_meters(381.0),
+        }),
+    };
+
+    let last_lrp = Point {
+        coordinate: Coordinate {
+            lon: 13.46284,
+            lat: 52.51500,
+        },
+        line: LineAttributes {
+            frc: Frc::Frc6,
+            fow: Fow::SingleCarriageway,
+            bearing: Bearing::from_degrees(17),
+        },
+        path: None,
+    };
+
+    let candidate_lines = [
+        CandidateLines {
+            lrp: first_lrp,
+            lines: vec![
+                CandidateLine {
+                    lrp: first_lrp,
+                    edge: EdgeId(8717174),
+                    rating: RatingScore::from(926.3),
+                    distance_to_projection: None,
+                },
+                CandidateLine {
+                    lrp: first_lrp,
+                    edge: EdgeId(4925291),
+                    rating: RatingScore::from(880.4),
+                    distance_to_projection: Some(Length::from_meters(141.6)),
+                },
+            ],
+        },
+        CandidateLines {
+            lrp: last_lrp,
+            lines: vec![CandidateLine {
+                lrp: last_lrp,
+                edge: EdgeId(109783),
+                rating: RatingScore::from(924.9),
+                distance_to_projection: None,
+            }],
+        },
+    ];
+
+    let routes = resolve_routes(&config, graph, &candidate_lines).unwrap();
+
+    assert_eq!(
+        routes,
+        [Route {
+            lrp: first_lrp,
+            length: Length::from_meters(379.0),
+            edges: vec![EdgeId(8717174), EdgeId(8717175), EdgeId(109783)]
+        }]
     );
 }
