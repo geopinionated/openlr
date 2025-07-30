529120cfa3976e1a593d5deafdbe91fd12e01b81
gliderkite <gliderkite@gmail.com>
2025-07-30 14:59:50 +0200

    Delete unused module

--- src/routing.rs
+++
@@ -1,5 +0,0 @@
-mod dijkstra; // TODO: rename shortest_path.rs and move to decoder module
-mod route;
-
-pub use dijkstra::{ShortestPathConfig, shortest_path};
-pub use route::{Path, Route, Routes};
